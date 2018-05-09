47bb2fb143f65092b4d77abcb2c1bcb48c9cd176
RubenOchoaPerez <33356523+RubenOchoaPerez@users.noreply.github.com>
2018-05-09 09:03:38 +0300

    Add files via upload

---
+++ thema2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+class cirlce{
+	protected: double radius;
+};
+class cylinder: public circle{
+	private:
+	double height;
+	double show(){
+		return 
+		}		
+		}	
